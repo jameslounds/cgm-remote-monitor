41d8a99c8bd1dd27222474cc73dd812cb7c5ac9a
James Lounds <jameslounds@gmail.com>
2024-12-03 12:42:18 +0000

    add test for pluginbase tooltips, test *all* plugin types have their pills rendered correctly

--- frontend/tests/pluginbase.test.js
+++ frontend/tests/pluginbase.test.js
@@ -2,18 +2,77 @@
  * @vitest-environment jsdom
  */
 
-import { describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
 import "@testing-library/jest-dom/vitest";
 
+import fs from "fs";
+import * as d3 from "d3";
+import path from "path";
+
+import initPluginBase from "../lib/plugins/pluginbase";
+
+const indexHTML = fs
+  .readFileSync(path.resolve("./bundle/index.html"))
+  .toString();
 
 describe("pluginbase", () => {
-  it("updates major pill text correctly", async () => {
-    await setupBrowser();
+  beforeEach(async (ctx) => {
+    document.write(indexHTML);
+    ctx.tooltip = d3
+      .select("body")
+      .append("div")
+      .attr("class", "tooltip")
+      .style("opacity", 0);
+    ctx.pluginBase = initPluginBase(
+      $(".majorPills"),
+      $(".minorPills"),
+      $(".statusPills"),
+      $(".bgStatus"),
+      ctx.tooltip
+    );
+  });
 
-    const bgStatusBefore = document.querySelector(".bgStatus");
-    const minorPillsBefore = document.querySelector(".minorPills");
+  it.for([
+    { pluginType: "pill-major", selector: ".majorPills" },
+    { pluginType: "pill-minor", selector: ".minorPills" },
+    { pluginType: "unrecognised", selector: ".minorPills" },
+    { pluginType: "pill-status", selector: ".statusPills" },
+    { pluginType: "bg-status", selector: ".bgStatus" },
+  ])(
+    "sets $pluginType pill correctly",
+    ({ pluginType, selector }, { pluginBase }) => {
+      const el = document.querySelector(selector);
+      const initialChildrenLength = el.children.length;
 
-    window.Nightscout.client.ctx.pluginBase.updatePillText(
+      pluginBase.updatePillText(
+        {
+          name: "fake",
+          label: "Insulin-on-Board",
+          pluginType: pluginType,
+        },
+        {
+          value: "123",
+          label: "TEST",
+          info: [{ label: "Label", value: "Value" }],
+        }
+      );
+
+      expect(el).toHaveTextContent("TEST123");
+      expect(el.children.length).toBe(initialChildrenLength + 1);
+
+      [".minorPills", ".majorPills", ".statusPills"].forEach((s) => {
+        if (selector !== s) {
+          expect(
+            document.querySelector(s).children.length,
+            `Expect no pills in ${s}`
+          ).toBe(0);
+        }
+      });
+    }
+  );
+
+  it("creates the tooltip correctly", ({ pluginBase, tooltip }) => {
+    pluginBase.updatePillText(
       {
         name: "fake",
         label: "Insulin-on-Board",
@@ -26,9 +85,14 @@ describe("pluginbase", () => {
       }
     );
 
-    expect(document.querySelector(".majorPills")).toHaveTextContent("TEST123");
+    const pillEl = document.querySelector("span.pill.fake");
 
-    expect(document.querySelector(".minorPills")).toBe(minorPillsBefore);
-    expect(document.querySelector(".bgStatus")).toBe(bgStatusBefore);
+    pillEl.dispatchEvent(new MouseEvent("mouseover"));
+
+    expect(tooltip.html()).toBe("<strong>Label</strong> Value");
+    expect(parseFloat(tooltip.node().style.opacity)).toBeCloseTo(0.9);
+
+    pillEl.dispatchEvent(new MouseEvent("mouseout"));
+    expect(parseFloat(tooltip.node().style.opacity)).toBe(0);
   });
 });
