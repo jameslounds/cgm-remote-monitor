f129c1e9523d3d2112f0907b768b8d0a45871e24
James Lounds <jameslounds@gmail.com>
2024-12-17 13:28:18 +0000

    migrate `profilefunctions` to a class with jsdoc types
    also adds package `@types/memory-cache`

--- frontend/global.d.ts
+++ frontend/global.d.ts
@@ -1,6 +1,7 @@
 declare global {
   interface ObjectConstructor {
     keys<T>(obj: T): (keyof T)[];
+    values<T>(obj: T): T[keyof T][];
   }
 }
 
--- frontend/lib/types.d.ts
+++ frontend/lib/types.d.ts
@@ -17,3 +17,29 @@ export type Snooze = NotifyBase & { lengthMills: number };
 
 export type Level =
   (typeof import("./constants"))[`LEVEL_${"URGENT" | "WARN" | "INFO" | "LOW" | "LOWEST" | "NONE"}`];
+
+export type Profile = {
+  defaultProfile?: string;
+  startDate?: string;
+  id?: string;
+  _id?: string;
+  convertedOnTheFly?: boolean;
+  mills?: number;
+  time?: string;
+  timeAsSeconds?: number;
+  store?: {
+    [K in string]: Omit<Profile, "store">;
+  };
+};
+
+export type Treatment = {
+  mills: number;
+  endmills: number;
+  duration?: number;
+  profile: string;
+  profileJson?: string;
+
+  relative?: number;
+  absolute?: number;
+  percent?: number;
+};
