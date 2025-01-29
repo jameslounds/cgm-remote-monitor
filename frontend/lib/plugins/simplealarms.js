1c5c6e974f1bc8111c2994a2bddf671e17408b01
James Lounds <jameslounds@gmail.com>
2025-01-29 13:28:28 +0000

    migrate simplealarms plugimn to class +)jsdoc
    Typescript was smart enough to notice that `levels.isAlarm(level: Level): level is Levels['URGENT'] | Levels['WARN']`, it isn't smart enough to realise that if `result.levels: WARN | URGENT`, then `reuslt.title: string` (not `string | undefined`). So unfortunately, I've split `levels.isAlarm` into `=== levels.WARN || === levels.URGENT`

--- frontend/lib/plugins/simplealarms.js
+++ frontend/lib/plugins/simplealarms.js
@@ -1,91 +1,112 @@
 "use strict";
 
-/** Server only? */
+/** @import {Plugin} from "../types" */
+/** @import {PluginCtx} from "." */
+/** @import {InitializedSandbox} from "../sandbox" */
 
-var times = require("../times");
+const times = require("../times");
 
-function init(ctx) {
-  var simplealarms = {
-    name: /** @type {const} */ ("simplealarms"),
-    label: "Simple Alarms",
-    pluginType: "notification",
-  };
+/**
+ * Server only?
+ *
+ * @implements {Plugin}
+ */
+class SimpleAlarmsPlugin {
+  name = /** @type {const} */ ("simplealarms");
+  label = "Simple Alarms";
+  pluginType = "notification";
 
-  var levels = ctx.levels;
+  /** @param {PluginCtx} ctx */
+  constructor(ctx) {
+    this.levels = ctx.levels;
+  }
 
-  simplealarms.checkNotifications = function checkNotifications(sbx) {
-    var lastSGVEntry = sbx.lastSGVEntry(),
-      scaledSGV = sbx.scaleEntry(lastSGVEntry);
+  /** @param {InitializedSandbox} sbx */
+  checkNotifications(sbx) {
+    const lastSGVEntry = sbx.lastSGVEntry();
+    const scaledSGV = sbx.scaleEntry(lastSGVEntry);
+
+    if (!scaledSGV || !lastSGVEntry) return;
     if (
-      scaledSGV &&
-      lastSGVEntry &&
-      lastSGVEntry.mgdl > 39 &&
-      sbx.time - lastSGVEntry.mills < times.mins(10).msecs
+      lastSGVEntry.mgdl <= 39 ||
+      sbx.time - lastSGVEntry.mills >= times.mins(10).msecs
     ) {
-      var result = simplealarms.compareBGToTresholds(scaledSGV, sbx);
-      if (levels.isAlarm(result.level)) {
-        sbx.notifications.requestNotify({
-          level: result.level,
-          title: result.title,
-          message: sbx.buildDefaultMessage(),
-          eventName: result.eventName,
-          plugin: simplealarms,
-          pushoverSound: result.pushoverSound,
-          debug: {
-            lastSGV: scaledSGV,
-            thresholds: sbx.settings.thresholds,
-          },
-        });
-      }
+      return;
     }
-  };
 
-  simplealarms.compareBGToTresholds = function compareBGToTresholds(
-    scaledSGV,
-    sbx,
-  ) {
-    var result = { level: levels.INFO };
+    const result = this.compareBGToTresholds(scaledSGV, sbx);
+    if (
+      result.level === this.levels.WARN ||
+      result.level === this.levels.URGENT
+    ) {
+      sbx.notifications.requestNotify({
+        level: result.level,
+        title: result.title,
+        message: sbx.buildDefaultMessage(),
+        eventName: result.eventName,
+        plugin: this,
+        pushoverSound: result.pushoverSound,
+        debug: {
+          lastSGV: scaledSGV,
+          thresholds: sbx.settings.thresholds,
+        },
+      });
+    }
+  }
+
+  /**
+   * @param {number} scaledSGV
+   * @param {InitializedSandbox} sbx
+   * @returns
+   */
+  compareBGToTresholds(scaledSGV, sbx) {
+    if (
+      sbx.settings.alarmUrgentLow &&
+      scaledSGV < sbx.scaleMgdl(sbx.settings.thresholds.bgLow)
+    ) {
+      return /** @type {const} */ ({
+        level: this.levels.URGENT,
+        title: this.levels.toDisplay(this.levels.URGENT) + " LOW",
+        pushoverSound: "persistent",
+        eventName: "low",
+      });
+    } else if (
+      sbx.settings.alarmLow &&
+      scaledSGV < sbx.scaleMgdl(sbx.settings.thresholds.bgTargetBottom)
+    ) {
+      return /** @type {const} */ ({
+        level: this.levels.WARN,
+        title: this.levels.toDisplay(this.levels.WARN) + " LOW",
+        pushoverSound: "falling",
+        eventName: "low",
+      });
+    }
 
     if (
       sbx.settings.alarmUrgentHigh &&
       scaledSGV > sbx.scaleMgdl(sbx.settings.thresholds.bgHigh)
     ) {
-      result.level = levels.URGENT;
-      result.title = levels.toDisplay(levels.URGENT) + " HIGH";
-      result.pushoverSound = "persistent";
-      result.eventName = "high";
+      return /** @type {const} */ ({
+        level: this.levels.URGENT,
+        title: this.levels.toDisplay(this.levels.URGENT) + " HIGH",
+        pushoverSound: "persistent",
+        eventName: "high",
+      });
     } else if (
       sbx.settings.alarmHigh &&
       scaledSGV > sbx.scaleMgdl(sbx.settings.thresholds.bgTargetTop)
     ) {
-      result.level = levels.WARN;
-      result.title = levels.toDisplay(levels.WARN) + " HIGH";
-      result.pushoverSound = "climb";
-      result.eventName = "high";
+      return /** @type {const} */ ({
+        level: this.levels.WARN,
+        title: this.levels.toDisplay(this.levels.WARN) + " HIGH",
+        pushoverSound: "climb",
+        eventName: "high",
+      });
     }
 
-    if (
-      sbx.settings.alarmUrgentLow &&
-      scaledSGV < sbx.scaleMgdl(sbx.settings.thresholds.bgLow)
-    ) {
-      result.level = levels.URGENT;
-      result.title = levels.toDisplay(levels.URGENT) + " LOW";
-      result.pushoverSound = "persistent";
-      result.eventName = "low";
-    } else if (
-      sbx.settings.alarmLow &&
-      scaledSGV < sbx.scaleMgdl(sbx.settings.thresholds.bgTargetBottom)
-    ) {
-      result.level = levels.WARN;
-      result.title = levels.toDisplay(levels.WARN) + " LOW";
-      result.pushoverSound = "falling";
-      result.eventName = "low";
-    }
-
-    return result;
-  };
-
-  return simplealarms;
+    return /** @type {const} */ ({ level: this.levels.INFO });
+  }
 }
 
-module.exports = init;
+/** @param {PluginCtx} ctx */
+module.exports = (ctx) => new SimpleAlarmsPlugin(ctx);
